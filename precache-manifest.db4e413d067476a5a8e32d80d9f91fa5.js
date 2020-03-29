aacde6acbd9eeefed7f426d8060fe447ae065805
anton-ua <antonyua23@gmail.com>
2020-03-29 13:11:04 +0300

    Updates

---
+++ precache-manifest.db4e413d067476a5a8e32d80d9f91fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbb981181d1d663159d621ea311f47b",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "61ba0b086b04d135b7b1",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.e08b41ef.chunk.css"
+  },
+  {
+    "revision": "8046c1a083eb2a8e6ef7",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d4beac89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d4beac89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ba0b086b04d135b7b1",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.850405f8.chunk.js"
+  },
+  {
+    "revision": "cc5d00c14a25a053fbb7",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.e35af0b9.js"
+  }
+]);
